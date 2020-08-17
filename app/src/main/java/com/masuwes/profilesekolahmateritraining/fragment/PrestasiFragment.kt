6b88d0e04da2ff163ea47668899b2d7448dbb5e4
Dzaky Fawwaz <dzaky.aryadii@gmail.com>
2020-08-17 08:36:58 +0700

    p

--- app/src/main/java/com/masuwes/profilesekolahmateritraining/fragment/PrestasiFragment.kt
+++ app/src/main/java/com/masuwes/profilesekolahmateritraining/fragment/PrestasiFragment.kt
@@ -1,60 +1,89 @@
 package com.masuwes.profilesekolahmateritraining.fragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.alespero.expandablecardview.ExpandableCardView
 import com.masuwes.profilesekolahmateritraining.R
+import kotlinx.android.synthetic.main.fragment_prestasi.*
+
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PrestasiFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PrestasiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+//    private var spinnerInter: Spinner? = null
+//    private var spinnerNas: Spinner? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+        savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_prestasi, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PrestasiFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PrestasiFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val card : ExpandableCardView = findViewById(R.id.profile)
+        val card = spinner_internasional
+
+        card.setOnExpandedListener { _, isExpanded ->
+            Toast.makeText(context, if(isExpanded) "Expanded!" else "Collapsed!", Toast.LENGTH_SHORT).show()
+        }
+
+////        spinnerInter?.onItemSelectedListener
+//        spinnerInter = view.findViewById(R.id.spinner_internasional)
+//        spinnerNas = view.findViewById(R.id.spinner_nasional)
+//        spinnerInter?.onItemSelectedListener
+//        spinnerNas?.onItemSelectedListener
+//
+//        context?.let {
+//            ArrayAdapter.createFromResource(it,
+//                R.array.isi_spinner,
+//                android.R.layout.simple_list_item_1
+//            ).also { adapter ->
+//                adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+//                spinnerInter?.adapter = adapter
+//            }
+//        }
+
     }
+
 }
+
+
+
+
+//    override fun onViewCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        val spinner : Spinner = spinner_internasional
+//        ArrayAdapter.createFromResource(this., R.array.isi_spinner, android.R.layout.simple_list_item_1).also { adapter->
+//            adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+//            spinner.adapter = adapter
+//
+//
+//        val v: View = inflater.inflate(R.layout.fragm, container, false)
+//
+//
+//        }
+//    }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_prestasi, container, false)
+//    }
+
